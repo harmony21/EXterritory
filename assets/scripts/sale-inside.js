e7e084a1d48d12ad5b3c4b87642246ce459735c5
Inna <21icelady97@gmail.com>
2018-09-25 10:01:11 +1000

    remarks in pc

--- assets/scripts/sale-inside.js
+++ assets/scripts/sale-inside.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     if ($(window).width() > 540) {
-        $('.sale-order-list-inner-container-item-text-title').mouseenter(function(ev) {
-        verItem = $(this).closest('.sale-order-list-inner-container-item-text-title'),
-        verItems = verItem.closest('.sale-order-list-inner-container-item').siblings().find('.sale-order-list-inner-container-item-text-title');
+        $('.sale-order-list-inner-container-item').click(function(ev) {
+        verItem = $(this).closest('.sale-order-list-inner-container-item'),
+        verItems = verItem.siblings();
         verContent = $(this).closest('.sale-order-list-inner-container-item').next('.order-wrap');
         verContents = verItems.closest('.sale-order-list-inner-container-item').next('.order-wrap');
 
